bc5270a42e781e5eac7b4781f50c92d9e9cb91f7
xenorock <MorrisLin1208@gmail.com>
2021-12-22 12:56:21 +0800

    Test02

--- Trace.py
+++ Trace.py
@@ -1,5 +1,8 @@
 import pandas as pd 
 
-data  = pd.read_csv('c:/Users/Morris/Desktop/MasterThesis/dataResult.csv')
-print ("git Test")
+data  = pd.read_csv('c:/Users/Morris/Desktop/MasterThesis/MasterThesis/dataResult.csv')
+print ("start")
+#data.info()
+#data['orderid'][0:4]
+
 print ("End ")
